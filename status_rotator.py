205092e390dac1a0d0517adc6747d9a5ca9ecd07
Paweł Żukowski <p.z.idlecode@gmail.com>
2019-10-11 18:09:59 +0200

    Add more universal status_rotator script

---
+++ status_rotator.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import argparse
+import os
+from time import sleep
+from RPLCD.gpio import CharLCD
+from RPi import GPIO
+
+def lcd_write(text):
+    lcd = CharLCD(
+            pin_rs=15, pin_rw=16, pin_e=18,
+            pins_data=[21, 22, 23, 24],
+            numbering_mode=GPIO.BOARD,
+            cols=20, rows=4, dotsize=8,
+            charmap='A02',
+            auto_linebreaks=True
+          )
+
+    lcd.clear()
+    lcd.write_string(text)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="LCD text rotation utility")
+    parser.add_argument('-d', '--delay', type=float, default=3.0, help="Delay between different messages in seconds")
+    parser.add_argument('directory', type=str, help="Path to the directory with messages")
+
+    args = parser.parse_args()
+    delay_seconds = args.delay
+    directory_path = args.directory
+    
+    delay_seconds = max(delay_seconds, .3)
+    
+    if not os.path.isdir(directory_path):
+        print("{} is not a directory".format(directory_path))
+        exit(1)
+    
+    while True:
+        message_files = sorted(os.listdir(directory_path))
+        message_files = map(lambda file_path: os.path.join(directory_path, file_path), message_files)
+        message_files = list(filter(os.path.isfile, message_files))
+        if len(message_files) == 0:
+            print("There is no files in message directory")
+            exit(1)
+
+        print("There are {} files in watched directory".format(len(message_files)))
+        for f in message_files:
+            print("Displaying " + f)
+            with open(f, 'r') as message_file:
+                message = message_file.read()
+                message = message.replace('\n', '\n\r')
+                lcd_write(message)
+            sleep(delay_seconds)
+
