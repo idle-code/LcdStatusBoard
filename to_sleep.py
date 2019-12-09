48b211b5d39099a5d5e5ec788d6ee4421e8d5bf1
Paweł Żukowski <p.z.idlecode@gmail.com>
2019-12-09 18:54:38 +0100

    Center to_sleep outputs

--- to_sleep.py
+++ to_sleep.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 from datetime import datetime, time, timedelta
 
+LCD_WIDTH = 16
+
+def center_print(text):
+    print(text.center(LCD_WIDTH))
+
 current_time = datetime.now()
 today = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
 sleep_time = today + timedelta(hours=21, minutes=30)
@@ -8,10 +13,10 @@ wakeup_time = sleep_time + timedelta(hours=8, minutes=30)
 
 if current_time < sleep_time:
     delta = (sleep_time - current_time)
-    print("Time to sleep:")
-    print("{}h {}m".format(delta.seconds//3600, (delta.seconds//60)%60))
+    center_print("Time to sleep")
+    center_print("{}h {}m".format(delta.seconds//3600, (delta.seconds//60)%60))
 else:
     delta = (wakeup_time - current_time)
-    print("Time to wake up:")
-    print("{}h {}m".format(delta.seconds//3600, (delta.seconds//60)%60))
+    center_print("Time to wake up")
+    center_print("{}h {}m".format(delta.seconds//3600, (delta.seconds//60)%60))
 
