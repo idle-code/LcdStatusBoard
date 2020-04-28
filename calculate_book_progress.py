08cc9339a564c95cff3028e33c88e6d7cf422e3f
Pawel Zukowski <p.z.idlecode@gmail.com>
2020-04-28 12:47:52 +0200

    Add days left display
    Remove Myslenie from tracking

--- calculate_book_progress.py
+++ calculate_book_progress.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from collections import namedtuple
 
 Book = namedtuple('Book', 'name total_pages')
@@ -10,17 +10,27 @@ class BookProgress:
         self.start_date = start_date
         self.start_page = start_page
         self.pages_per_day = pages_per_day
+        self.end_date = start_date + timedelta(days=(self.book.total_pages - self.start_page) // self.pages_per_day)
 
     def _calculate_progress(self):
         today_date = datetime.now().date()
         days_from_start = (today_date - self.start_date).days
         expected_page_today = self.start_page + days_from_start * self.pages_per_day
-        return expected_page_today
+        days_left = (self.end_date - today_date).days
+        return expected_page_today, days_left
 
     def __str__(self):
-        page_today = self._calculate_progress()
+        page_today, days_left = self._calculate_progress()
         progress_fraction = page_today / self.book.total_pages
-        return "{}: p{} ({:.0%})".format(self.book.name, page_today, progress_fraction)
+        if days_left > 0:
+            #ends_message = "\nEnds in {} days ({})".format(days_left, self.end_date)
+            ends_message = "\nEnds in {} days".format(days_left)
+        elif days_left == 0:
+            ends_message = "\nEnds today"
+        else:
+            ends_message = "\nEnded {} days ago".format(-days_left)
+
+        return "{}: p{} ({:.0%})".format(self.book.name, page_today, progress_fraction) + ends_message
 
 
 stallings = Book(name='Stallings', total_pages=818)
@@ -31,13 +41,4 @@ stallings_progress = BookProgress(
         pages_per_day = 2)
 
 print(stallings_progress)
-
-maththink = Book(name='Myslenie', total_pages=144)
-maththink_progress = BookProgress(
-        maththink,
-        start_date = date(2020, 1, 1),
-        start_page = 5,
-        pages_per_day = 2)
-
-print(maththink_progress)
 
