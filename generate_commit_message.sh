6fa96f1f68b3e79f05128f76db0527df98d74791
Paweł Żukowski <p.z.idlecode@gmail.com>
2020-10-20 15:02:35 +0200

    Make j2 path cron-compatible in generate_commit_message

--- generate_commit_message.sh
+++ generate_commit_message.sh
@@ -2,5 +2,5 @@
 TEMPLATE="$(dirname $0)/commit_message.txt"
 #COMMIT_COUNTER_URL="http://localhost:5000/?json"
 COMMIT_COUNTER_URL="https://europe-west3-idlecode-va-data-collection.cloudfunctions.net/count_commits/?json"
-curl "$COMMIT_COUNTER_URL" | j2 --format json "$TEMPLATE"
+curl --silent "$COMMIT_COUNTER_URL" | /usr/local/bin/j2 --format json "$TEMPLATE"
 
